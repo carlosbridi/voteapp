3ad06e48153f6a882dc1566de16f8a15c4ffe40a
Carlos Bridi <carlosbridi@gmail.com>
2023-07-04 16:43:38 -0300

    chore: adjust on controllers and versions route

--- src/main/java/br/com/vote/controllers/AgendaController.java
+++ src/main/java/br/com/vote/controllers/AgendaController.java
@@ -1,6 +1,7 @@
 package br.com.vote.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,14 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.vote.documents.Agenda;
 import br.com.vote.http.input.AgendaRequest;
 import br.com.vote.service.AgendaService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-@RestController("/v1/pauta")
+@RestController("/pauta")
 public class AgendaController {
 
 	@Autowired
 	private AgendaService pautaService;
 	
-	@PostMapping(value = "/create")
+	@PostMapping(value = "/v1/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Agenda created")
+	})
 	public ResponseEntity<Agenda> createPauta(@RequestBody AgendaRequest agendaRequest){
 		return ResponseEntity.ok(pautaService.createAgenda(agendaRequest));
 	}
--- src/main/java/br/com/vote/controllers/VoteController.java
+++ src/main/java/br/com/vote/controllers/VoteController.java
@@ -1,6 +1,7 @@
 package br.com.vote.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.vote.http.input.VoteRequest;
 import br.com.vote.service.VoteService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController("/vote")
 public class VoteController {
@@ -17,7 +19,13 @@ public class VoteController {
 	@Autowired
 	private VoteService voteService;
 	
-	@PutMapping(value = "v1/{agendaId}/vote/{associatedDocument}")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Vote registered with success"),
+		@ApiResponse(responseCode = "404", description = "Agenda not found"),
+		@ApiResponse(responseCode = "502", description = "Error with request"),
+		
+	})
+	@PutMapping(value = "/v1/{agendaId}/vote/{associatedDocument}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity registerVote(@PathVariable String agendaId, @PathVariable String associatedDocument, @RequestBody VoteRequest voteRequest) {
 		voteService.vote(agendaId, associatedDocument, voteRequest.getVote().toString());
 		return ResponseEntity.ok(null);
